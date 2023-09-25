081b086b69308f2ad27d8d695d414a0635ebd766
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-09-25 20:24:13 +0900

    Project Init

---
+++ BookKeep/BookKeep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookKeep
+//
+//  Created by Alex Cho on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        print(#file, #function)
+    }
+
+
+}
+
