783513ca004690da8b77e22ea37dd41aa8d580ed
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-12-23 10:23:17 +0900

    Widget Extension Init

---
+++ BookKeep/WidgetLatestBook/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  WidgetLatestBook
+//
+//  Created by Alex Cho on 12/23/23.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
